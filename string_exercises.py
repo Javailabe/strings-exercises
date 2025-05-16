012464ce245c6dfe03020bc888eeffb3d3d8b020
Javailabe <kulecki.pawel@gmail.com>
2025-05-16 16:31:07 +0200

    zadania 1-3

---
+++ string_exercises.py
@@ -0,0 +1,20 @@
+# # zad 1, first letter capital
+# # text = input('Podaj tekst: ')
+# text = 'python'
+# modified_text = text[0].upper() + text[1:]
+# print(modified_text)
+
+# # zad 2, count a
+# count = 0
+# # text = input('Podaj tekst: ')
+# text = 'ananas'
+# for char in text:
+#     if char == 'a':
+#         count += 1
+# print('a wystepuje: ' + str(count))
+
+# # zad 3, revert
+# # text = input('Podaj tekst: ')
+# text = 'kot'
+# reversed_text = text[::-1]
+# print(reversed_text)
